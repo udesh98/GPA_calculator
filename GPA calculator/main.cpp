3762627f5a8d49de5dad71f2dd18b25132b6ff17
Udesh Maduranga <um4me.98@gmail.com>
2021-09-26 12:59:18 +0530

    files added successfully!

---
+++ GPA calculator/main.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include<string.h>
+#include<stdlib.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+int n;
+
+void cal(string arr1[], int arr2[]){
+	
+	double sum = 0.0;
+	int credit = 0;
+	double gpa;
+	int x;
+	
+	for(x=0; x<n ; x++){
+		
+		if(arr1[x]=="A+" || arr1[x]=="a+" || arr1[x]=="A" || arr1[x]=="a"){
+			sum = sum + 4.0*arr2[x];	
+		}
+		
+		else if(arr1[x]=="A-" || arr1[x]=="a-"){
+			sum = sum + 3.7*arr2[x];	
+		}
+		
+		else if(arr1[x]=="B+" || arr1[x]=="b+"){
+			sum = sum + 3.3*arr2[x];	
+		}
+		
+		else if(arr1[x]=="B" || arr1[x]=="b"){
+			sum = sum + 3.0*arr2[x];	
+		}
+		
+		else if(arr1[x]=="B-" || arr1[x]=="b-"){
+			sum = sum + 2.7*arr2[x];	
+		}
+		
+		else if(arr1[x]=="C+" || arr1[x]=="c+"){
+			sum = sum + 2.3*arr2[x];	
+		}
+		
+		else if(arr1[x]=="C" || arr1[x]=="c"){
+			sum = sum + 2.0*arr2[x];	
+		}
+		
+		else if(arr1[x]=="C-" || arr1[x]=="c-"){
+			sum = sum + 1.7*arr2[x];	
+		}
+		
+		else if(arr1[x]=="D+" || arr1[x]=="d+"){
+			sum = sum + 1.3*arr2[x];	
+		}
+		
+		else if(arr1[x]=="D" || arr1[x]=="d"){
+			sum = sum + 1.0*arr2[x];	
+		}
+		
+		else{
+			sum = sum + 0.0;
+		}
+	}
+	
+	for(x=0; x<n; x++){
+		credit = credit + arr2[x];
+	}
+	
+	gpa =  sum/credit;
+	cout << endl << "***** " "Your GPA is " << gpa << " *****" << endl;
+	
+}
+
+int main(){
+	
+	int x;
+	cout << "***** ~Find out your GPA here~ *****" << endl << endl;
+	cout << "Enter the number of subjects : ";
+	cin >> n; cout << endl;
+	
+	string array1[n];
+	for (x=0; x<n; x++) {
+		cout << "Enter the " << "subject "  << x+1 << " grade : ";
+    	cin >> array1[x];
+	}
+	
+	cout << endl;
+	
+	int array2[n];
+	for (x=0; x<n; x++) {
+		cout << "Enter the " << "subject "  << x+1 << " credit value : ";
+    	cin >> array2[x];
+	}
+	
+	cal(array1, array2);
+	
+	system("pause");
+	
+}
